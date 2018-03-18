301346924c648c64d2bb50d40279df244a6d40f7
lkkbrian <35850909+lkkbrian@users.noreply.github.com>
2018-03-17 22:49:33 -0700

    Update doctor.ino

--- doctor.ino
+++ doctor.ino
@@ -2,7 +2,6 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  
   delay(500);                       
